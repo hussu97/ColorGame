c8e0c96a37950033da703c209d04cd58f4179775
hussu97 <h_abbasi97@hotmail.com>
2018-01-22 13:50:15 +0400

    Formatted buttons and fonts Added transitions in colorGame.css

--- colorGame.js
+++ colorGame.js
@@ -1,5 +1,6 @@
 // Declare private attributes
-var colors = generateRandomColors(6);
+var numberOfSquares=6;
+var colors = generateRandomColors(numberOfSquares);
 var squares = document.querySelectorAll(".square");
 var pickedColor = pickColor();
 var colorDisplay =document.getElementById("colorDisplay");
@@ -7,6 +8,8 @@ var clickedColor;
 var messageDisplay = document.querySelector("#message");
 var playAgainButton=document.querySelector("#reset");
 var heading=document.querySelector("h1");
+var easyButton=document.getElementById("easy");
+var hardButton=document.getElementById("hard");
 
 colorDisplay.textContent=pickedColor;
 
@@ -31,7 +34,7 @@ playAgainButton.addEventListener("click",function(){
   //change button text
   this.textContent="New Colors";
   //generate new color
-  colors=generateRandomColors(6);
+  colors=generateRandomColors(numberOfSquares);
   //pick a new color from the array for colorPicked
   colorDisplay.textContent=pickedColor=pickColor();
   //change colors of squares
@@ -39,9 +42,45 @@ playAgainButton.addEventListener("click",function(){
     squares[i].style.backgroundColor=colors[i];
   }
   //Reset button of h1
-  heading.style.backgroundColor="#232323";
+  heading.style.backgroundColor="steelblue";
 })
 
+easyButton.addEventListener("click",function(){
+  //Changing the selected mode
+  heading.style.backgroundColor="steelblue";
+  this.classList.add("selected");
+  hardButton.classList.remove("selected");
+  //generate new color
+  numberOfSquares=3;
+  colors=generateRandomColors(numberOfSquares);
+  //pick a new color from the array for colorPicked
+  colorDisplay.textContent=pickedColor=pickColor();
+  //change colors of squares
+  for(var i=0;i<squares.length;i++){
+    if(colors[i]){
+      squares[i].style.backgroundColor=colors[i];
+    }else{
+      squares[i].style.display="none";
+    }
+  }
+})
+
+hardButton.addEventListener("click",function(){
+  //Changing the selected mode
+  heading.style.backgroundColor="steelblue";
+  this.classList.add("selected");
+  easyButton.classList.remove("selected");
+  //generate new color
+  numberOfSquares=6;
+  colors=generateRandomColors(numberOfSquares);
+  //pick a new color from the array for colorPicked
+  colorDisplay.textContent=pickedColor=pickColor();
+  //change colors of squares
+  for(var i=0;i<squares.length;i++){
+    squares[i].style.display="block";
+      squares[i].style.backgroundColor=colors[i];
+  }
+})
 function changeColors(color){
   //loop through all the squares and change color to match given color
   for(var i=0;i<squares.length;i++){
