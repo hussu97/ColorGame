313a5d7d937e42bb4ed18d61076f787393ae0e1d
hussu97 <h_abbasi97@hotmail.com>
2018-01-22 08:32:55 +0400

    Added random color functionality, and added message display to show whether user has won the game or not

--- colorGame.js
+++ colorGame.js
@@ -1,16 +1,10 @@
 // Declare private attributes
-var colors = [
-  "rgb(255, 0, 0)",
-  "rgb(255, 255, 0)",
-  "rgb(0, 255, 0)",
-  "rgb(0, 255, 255)",
-  "rgb(0, 0, 255)",
-  "rgb(255, 0, 255)"
-];
+var colors = generateRandomColors(6);
 var squares = document.querySelectorAll(".square");
-var pickedColor =colors[3];
+var pickedColor = pickColor();
 var colorDisplay =document.getElementById("colorDisplay");
 var clickedColor;
+var messageDisplay = document.querySelector("#message");
 
 colorDisplay.textContent=pickedColor;
 
@@ -21,9 +15,39 @@ for(var i=0;i<squares.length;i++){
   squares[i].addEventListener("click",function(){
     clickedColor=this.style.backgroundColor;
     if(clickedColor===pickedColor){
-
+      messageDisplay.textContent="Good job!";
+      changeColors(clickedColor);
     }else{
-
+      this.style.backgroundColor="#232323";
+      messageDisplay.textContent="Try Again!";
     }
   });
+}
+
+function changeColors(color){
+  //loop through all the squares and change color to match given color
+  for(var i=0;i<squares.length;i++){
+    squares[i].style.backgroundColor=color;
+  }
+}
+
+function pickColor(){
+  return colors[Math.floor(Math.random()*colors.length)];
+}
+
+function generateRandomColors(number){
+  //Make array
+  var arr=[];
+  var colorString;
+  //add random numbers to array
+  for(var i=0;i<number;i++){
+    colorString="rgb(";
+    colorString+=Math.floor(Math.random()*256)+", ";
+    colorString+=Math.floor(Math.random()*256)+", ";
+    colorString+=Math.floor(Math.random()*256)+")";
+    arr.push(colorString);
+  }
+  //return array
+  return arr;
+
 }
