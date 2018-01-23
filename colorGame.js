b05b9635a1ad1cbc7b20757df6cf290d50dd5520
hussu97 <h_abbasi97@hotmail.com>
2018-01-23 23:37:57 +0400

    Final version
    Refactoring of functions in js file

--- colorGame.js
+++ colorGame.js
@@ -1,86 +1,88 @@
 // Declare private attributes
 var numberOfSquares=6;
-var colors = generateRandomColors(numberOfSquares);
-var squares = document.querySelectorAll(".square");
-var pickedColor = pickColor();
-var colorDisplay =document.getElementById("colorDisplay");
+var colors = [];
+var pickedColor;
 var clickedColor;
+var squares = document.querySelectorAll(".square");
+var colorDisplay =document.getElementById("colorDisplay");
 var messageDisplay = document.querySelector("#message");
 var playAgainButton=document.querySelector("#reset");
 var heading=document.querySelector("h1");
-var easyButton=document.getElementById("easy");
-var hardButton=document.getElementById("hard");
+var modeButtons=document.querySelectorAll(".mode");
 
-colorDisplay.textContent=pickedColor;
+init();
 
-for(var i=0;i<squares.length;i++){
-  //Add colors to squares
-  squares[i].style.backgroundColor=colors[i];
-  //Adding listeners to squares
-  squares[i].addEventListener("click",function(){
-    clickedColor=this.style.backgroundColor;
-    if(clickedColor===pickedColor){
-      messageDisplay.textContent="Good job!";
-      changeColors(clickedColor);
-      playAgainButton.textContent="Play Again?";
-    }else{
-      this.style.backgroundColor="#232323";
-      messageDisplay.textContent="Try Again!";
-    }
-  });
+//initializing the webpage
+function init(){
+  //mode butttons event listeners
+  setUpModeButtons();
+  //Square event listener
+  setUpSquares();
+  //Play button event listener
+  setUpPlayButton();
+  reset();
 }
 
-playAgainButton.addEventListener("click",function(){
-  //change button text
-  this.textContent="New Colors";
-  //generate new color
-  colors=generateRandomColors(numberOfSquares);
-  //pick a new color from the array for colorPicked
-  colorDisplay.textContent=pickedColor=pickColor();
-  //change colors of squares
-  for(var i=0;i<colors.length;i++){
-    squares[i].style.backgroundColor=colors[i];
-  }
-  //Reset button of h1
-  heading.style.backgroundColor="steelblue";
-})
+function setUpModeButtons(){
+  for(var i=0;i<modeButtons.length;i++){
+    modeButtons[i].addEventListener("click",function(){
+      modeButtons[0].classList.remove("selected");
+      modeButtons[1].classList.remove("selected");
+      this.classList.add("selected");
 
-easyButton.addEventListener("click",function(){
-  //Changing the selected mode
-  heading.style.backgroundColor="steelblue";
-  this.classList.add("selected");
-  hardButton.classList.remove("selected");
+      this.textContent==="Easy"?numberOfSquares=3:numberOfSquares=6;
+      reset();
+    })
+  }
+}
+
+function setUpSquares(){
+  for(var i=0;i<squares.length;i++){
+    //Adding listeners to squares
+    squares[i].addEventListener("click",function(){
+      clickedColor=this.style.backgroundColor;
+      if(clickedColor===pickedColor){
+        messageDisplay.textContent="Good job!";
+        changeColors(clickedColor);
+        playAgainButton.textContent="Play Again?";
+      }else{
+        this.style.backgroundColor="#232323";
+        messageDisplay.textContent="Try Again!";
+      }
+    });
+  }
+}
+
+function setUpPlayButton(){
+  playAgainButton.addEventListener("click",function(){
+    reset();
+  })
+}
+
+function reset(){
+  //change button text
+  playAgainButton.textContent="New Colors";
+  messageDisplay.textContent="";
   //generate new color
-  numberOfSquares=3;
   colors=generateRandomColors(numberOfSquares);
   //pick a new color from the array for colorPicked
   colorDisplay.textContent=pickedColor=pickColor();
   //change colors of squares
   for(var i=0;i<squares.length;i++){
     if(colors[i]){
+      squares[i].style.display="block";
       squares[i].style.backgroundColor=colors[i];
     }else{
       squares[i].style.display="none";
     }
   }
-})
-
-hardButton.addEventListener("click",function(){
-  //Changing the selected mode
+  //Reset color of h1
   heading.style.backgroundColor="steelblue";
-  this.classList.add("selected");
-  easyButton.classList.remove("selected");
-  //generate new color
-  numberOfSquares=6;
-  colors=generateRandomColors(numberOfSquares);
-  //pick a new color from the array for colorPicked
-  colorDisplay.textContent=pickedColor=pickColor();
-  //change colors of squares
-  for(var i=0;i<squares.length;i++){
-    squares[i].style.display="block";
-      squares[i].style.backgroundColor=colors[i];
-  }
-})
+}
+
+//===================================
+//Helper functions
+//===================================
 function changeColors(color){
   //loop through all the squares and change color to match given color
   for(var i=0;i<squares.length;i++){
